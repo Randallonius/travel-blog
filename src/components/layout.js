1e12d70000f6bc882248771bacc9248c604425fb
Randallonius <jonathanprandall@gmail.com>
2019-10-22 20:54:58 +0200

    initial styles setup

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,16 +1,49 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.org/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
+import { ThemeProvider } from 'emotion-theming'
+import { Global, css } from '@emotion/core'
 
 import Header from "./header"
 import "./layout.css"
+import { theme, reset } from '../styles'
+import 'typeface-poppins'
+
+const globalStyle = css`
+  ${reset}
+  h1, h2, h3, h4, h5, h6 {
+    color: ${theme.colors.primary};
+    font-family: poppins;
+  }
+  html {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
+  body {
+    color: ${theme.colors.primary};
+    background-color: ${theme.colors.bg};
+    font-family: poppins;
+  }
+  ::selection {
+    color: ${theme.colors.bg};
+    background-color: ${theme.colors.primary};
+  }
+  a {
+    color: ${theme.colors.greyDark};
+    transition: all 0.25s ease-in-out;
+    text-decoration: none;
+    font-weight: 700;
+    &:hover,
+    &:focus {
+      color: ${theme.colors.greyHover};
+      text-decoration: none;
+    }
+  }
+  p {
+    font-size: 1em;
+    line-height: 1.5em;
+  }
+`
 
 const Layout = ({ children }) => {
   const data = useStaticQuery(graphql`
@@ -24,25 +57,19 @@ const Layout = ({ children }) => {
   `)
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
-        <aside>This is the aside</aside>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
-      </div>
-    </>
+    <ThemeProvider theme={theme}>
+      <>
+        <Global styles={globalStyle} />
+        <Header siteTitle={data.site.siteMetadata.title} />
+          <main>{children}</main>
+          <aside>This is the aside</aside>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+      </>
+    </ThemeProvider>
   )
 }
 
---
+++ src/styles/theme.js
@@ -0,0 +1,22 @@
+const theme = {
+  colors: {
+    bg: '#f5f5f5',
+    black: '#000',
+    greyLight: '#EBEDF2',
+    grey: '#595C62',
+    greyHover: '#777777',
+    greyDark: '#303643',
+    primary: '#161616',
+    white: '#fff'
+  },
+  maxWidth: '1300px',
+  maxWidthText: '900px',
+  breakpoints: {
+    xs: '400px',
+    s: '600px',
+    m: '900px',
+    l: '1350px',
+  },
+}
+
+export default theme
