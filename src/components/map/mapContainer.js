bc137f39cdbbf3a68cb66e55e886b0022d837c92
Jonathan Randall <jonathanprandall@gmail.com>
2019-10-31 14:16:49 +0100

    Map implementation (#7)
    * integrated leaflet map into sidemenu
    
    * moved map components into folder

---
+++ src/components/map/mapContainer.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { useStaticQuery, graphql } from "gatsby"
+import LeafletMap from './map'
+
+const MapContainer = () => {
+  const data = useStaticQuery(graphql`
+    query MapData {
+      allPrismicGeo(sort: {order: ASC, fields: id}) {
+        edges {
+          node {
+            data {
+              coordinate_marker {
+                text
+              }
+              coordinate_title {
+                text
+              }
+              coordinates {
+                latitude
+                longitude
+              }
+            }
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <div>
+    {typeof window !== 'undefined' && <LeafletMap mapData={data.allPrismicGeo}/>}
+    </div>
+  )
+}
+
+export default MapContainer
