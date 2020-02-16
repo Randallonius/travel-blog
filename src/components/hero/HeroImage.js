1794da68ba2e7b5f51402dfa1216ff559fb67251
Randallonius <jonathanprandall@gmail.com>
2020-02-16 14:39:35 +0530

    removed hero slice

--- src/components/hero/HeroImage.js
+++ src/components/hero/HeroImage.js
@@ -1,12 +1,18 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
-import { Hero } from '../../slices'
+import Img from 'gatsby-image'
 
 class HeroImage extends Component {
   render() {
     const { data } = this.props
-    const slice = data.map(s => (s.slice_type && s.slice_type === 'hero_image' ? <Hero key={s.id} input={s} /> : null))
-    return <div>{slice}</div>
+    const imageData = data.map(s =>
+      s.slice_type && s.slice_type === 'hero_image' ? (
+        <div>
+          <Img key={s.id} fluid={s.primary.image.localFile.childImageSharp.fluid} />
+        </div>
+      ) : null
+    )
+    return <div>{imageData}</div>
   }
 }
 
--- src/slices/index.js
+++ src/slices/index.js
@@ -1,6 +1,5 @@
 import BodyText from './BodyText'
 import Image from './Image'
 import ExcerptSlice from './ExcerptSlice'
-import Hero from './Hero'
 
-export { Image, BodyText, Hero, ExcerptSlice }
+export { Image, BodyText, ExcerptSlice }
